636054c3b9051fd68512b030729b56efc238cd5b
Nikita <n.metelkin@madcrush.ru>
2018-07-11 17:47:38 +0300

    added basic methods

---
+++ messaging/test.js
@@ -0,0 +1,36 @@
+const Messenger = require ('./index.js');
+
+const chat_protocol = '/chat/1.0.0';
+const main_node_channel = 'news';
+const mainNodeId = 'QmYcuVrDn76jLz62zAQDmfttX9oSFH1cGXSH9rdisbHoGP';
+const mainNodeIp = '192.168.1.12';
+const mainNodeAddr = '/ip4/'+mainNodeIp+'/tcp/10333/ipfs/'+mainNodeId;
+
+const config = {
+    main_func: function (messenger) {
+        messenger.handle(chat_protocol,(protocol, conn) => {
+            console.log("start handling");
+            messenger.read_msg((msg)=>{
+                console.log("msg: " + msg);
+                messenger.send_msg("received msg: " + msg,conn);
+            },conn);
+        });
+
+        messenger.pubsub(main_node_channel,(data) => {
+            console.log(data);
+        });
+
+        messenger.dial(mainNodeAddr,(conn)=>{});
+
+
+        // messenger.dial_protocol(addr,protocol,(conn)=>{
+        //     messenger.read_msg((msg)=>{
+        //         console.log(msg);
+        //     },conn);
+        //     messenger.send_msg("I've been connected to you",conn);
+        // });
+    }
+};
+
+let messenger = new Messenger('./id');
+messenger.node_start(config);
